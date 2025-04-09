a6c9ad1c9fa3a81480a5b387c88f812d53820473
Chanchal Roy <croy7667@gmail.com>
2025-04-10 00:01:18 +0530

    Update RPS_Game.py

--- RPS_Game.py
+++ RPS_Game.py
@@ -1,15 +1,15 @@
-# ROCK PAPER SECISSOR GAME
+# ROCK PAPER SCISSORS GAME
 # DEVLOPER : CHANCHAL ROY
 
 from random import choice
-values = ['ROCK','PAPER','SECISSOR']
+values = ['ROCK','PAPER','SCISSORS']
 
 def test(my_hand):
     com_hand = choice(values)
     if (my_hand == 'R' or my_hand == 'r') and com_hand == 'PAPER':
         return 'COM',com_hand
 
-    elif (my_hand == 'R' or my_hand == 'r') and com_hand == 'SECISSOR':
+    elif (my_hand == 'R' or my_hand == 'r') and com_hand == 'SCISSORS':
         return 'ME',com_hand
 
     elif (my_hand == 'R' or my_hand == 'r') and com_hand == 'ROCK':
@@ -18,7 +18,7 @@ def test(my_hand):
     elif (my_hand == 'P' or my_hand == 'p') and com_hand == 'ROCK':
         return 'ME',com_hand
 
-    elif (my_hand == 'P' or my_hand == 'p') and com_hand == 'SECISSOR':
+    elif (my_hand == 'P' or my_hand == 'p') and com_hand == 'SCISSORS':
         return 'COM',com_hand
 
     elif (my_hand == 'P' or my_hand == 'p') and com_hand == 'PAPER':
@@ -30,7 +30,7 @@ def test(my_hand):
     elif (my_hand == 'S' or my_hand == 's') and com_hand == 'PAPER':
         return 'ME',com_hand
 
-    elif (my_hand == 'S' or my_hand == 's') and com_hand == 'SECISSOR':
+    elif (my_hand == 'S' or my_hand == 's') and com_hand == 'SCISSORS':
         return 'DRAW',com_hand
 
     else:
@@ -38,7 +38,7 @@ def test(my_hand):
         return 'None'
 
 while True:
-    my_hand = input('R for ROCK/P for PAPER/S for SECISSOR: ')
+    my_hand = input('R for ROCK/P for PAPER/S for SCISSORS: ')
     data = test(my_hand)
     if data[0] == 'ME': print(f'You are the Winner! Computer says {data[1]}.\n')
     elif data[0] == 'COM' : print(f"Computer is the Winner! It says {data[1]}.\n")
